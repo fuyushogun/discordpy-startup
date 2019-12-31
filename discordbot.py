fa40203f4c3ac0afa362aeb0096f25f8463526b5
fuyushogun <neongenesis0312@gmail.com>
2019-12-31 14:30:10 +0900

    ねこ？

--- discordbot.py
+++ discordbot.py
@@ -397,6 +397,27 @@ async def on_message(message):
     if re.search("明けまして", message.content):
         await message.channel.send(happy_holiday )
 
+    elif message.content == "!猫":
+        photo = random.choice(("https://i1.wp.com/memorynator.com/wp-content/uploads"
+                               "/2017/03/catgif-matomegif-91.gif?resize=296%2C311&ssl=1",
+                               "https://i1.wp.com/memorynator.com/wp-content/uploads"
+                               "/2017/03/catgif-matomegif-86.gif?resize=471%2C279&ssl=1",
+                               "https://i2.wp.com/memorynator.com/wp-content"
+                               "/uploads/2017/03/catgif-matomegif-68.gif?resize=500%2C282&ssl=1",
+                               "https://i0.wp.com/memorynator.com/wp-content"
+                               "/uploads/2017/03/catgif-matomegif-56.gif?resize=259%2C431&ssl=1",
+                               "https://i1.wp.com/memorynator.com/wp-content"
+                               "/uploads/2017/03/catgif-matomegif-35.gif?resize=498%2C283&ssl=1",
+                               "https://i1.wp.com/memorynator.com/wp-content"
+                               "/uploads/2017/03/catgif-matomegif-35.gif?resize=498%2C283&ssl=1"))
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(photo) as resp:
+                if resp.status != 200:
+                    return await message.channel.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await message.channel.send(file=discord.File(data,"cool_image.gif"))
+
 
 
     #print("処理の最後に次の式を追加します：")
@@ -407,6 +428,12 @@ async def on_message(message):
 @bot.command()
 async def chinpo(ctx):
     await ctx.send('ちんぽーー')
+
+
+
+
+
+
 
 
 bot.run(token)
